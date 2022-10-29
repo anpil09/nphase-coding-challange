2d4c11c1c036fe1fb4acb686817c8a8430067faf
Andrei Pilipenka <Andre.Pilipenko@x5.ru>
2022-10-29 14:46:09 +0300

    Implemented task 2

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,30 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 
 public class ShoppingCartService {
 
+    private static final int MIN_PRODUCT_QUANTITY_FOR_DISCOUNT = 4;
+    private static final BigDecimal DISCOUNT_FACTOR = BigDecimal.valueOf(0.9);
+
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
         return shoppingCart.getProducts()
                 .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
+                .map(this::calculateProductPrice)
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    private BigDecimal calculateProductPrice(Product product) {
+        BigDecimal priceWithoutDiscount = product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity()));
+
+        if (product.getQuantity() >= MIN_PRODUCT_QUANTITY_FOR_DISCOUNT) {
+            return priceWithoutDiscount.multiply(DISCOUNT_FACTOR);
+        }
+
+        return priceWithoutDiscount;
     }
 }
--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -6,7 +6,6 @@ import com.nphase.entity.ShoppingCart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
@@ -23,6 +22,22 @@ public class ShoppingCartServiceTest {
         BigDecimal result = service.calculateTotalPrice(cart);
 
         Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
+    }
+
+    @Test
+    public void calculatePriceWithDiscount() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.0), 5),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 3)
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        assertStrippingTrailingZeros(result.stripTrailingZeros(), BigDecimal.valueOf(33.0).stripTrailingZeros());
+    }
+
+    private void assertStrippingTrailingZeros(BigDecimal expected, BigDecimal actual) {
+        Assertions.assertEquals(expected.stripTrailingZeros(), actual.stripTrailingZeros());
     }
 
 }
